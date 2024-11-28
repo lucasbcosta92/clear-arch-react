fd952ed5b2b471442deecf89ccf78c295777e889
lucasbcosta92 <lucasbcosta92@gmail.com>
2024-11-27 22:43:53 -0300

    refactor: move axios mock to a sepate file

---
+++ src/infra/test/mock-axios.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { faker } from '@faker-js/faker'
+
+export const mockAxios = (): jest.Mocked<typeof axios> => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+
+  const mockedAxiosResult = {
+    data: faker.string.sample(),
+    status: faker.internet.httpStatusCode(),
+    statusText: 'OK',
+    headers: {},
+    config: {
+      url: ''
+    }
+  }
+
+  mockedAxios.post.mockResolvedValue(mockedAxiosResult)
+
+  return mockedAxios
+}
