52d11b33c5a1bfe3fcd87f649b7507892079c7cf
lucasbcosta92 <lucasbcosta92@gmail.com>
2024-12-14 15:53:11 -0300

    chore: add jest setup

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,10 @@ module.exports = {
   coverageDirectory: 'coverage',
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
-    '\\.scss$': 'indentity-obj-proxy'
+    '\\.scss$': 'identity-obj-proxy'
   },
   roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
