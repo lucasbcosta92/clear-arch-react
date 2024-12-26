f5206132355bfda2a051c1e567c143bf21b346a5
lucasbcosta92 <lucasbcosta92@gmail.com>
2024-12-26 12:40:25 -0300

    test: ensure RequiredFieldValidation returns false if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
+import { faker } from '@faker-js/faker/.'
 
 describe('RequiredFieldValidation', () => {
   test('should return error if field is empty', () => {
@@ -8,5 +9,13 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+
+    const error = sut.validate(faker.string.uuid())
+
+    expect(error).toBeFalsy()
   })
 })
