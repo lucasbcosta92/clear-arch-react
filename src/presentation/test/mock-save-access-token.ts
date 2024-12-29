4cf1aba41cb20914c464c69d619d3b3026826bab
lucasbcosta92 <lucasbcosta92@gmail.com>
2024-12-29 15:13:02 -0300

    test: ensure Login calls SaveAccessToken with correct value

---
+++ src/presentation/test/mock-save-access-token.ts
@@ -0,0 +1,9 @@
+import { type SaveAccessToken } from '@/domain/use-cases'
+
+export class SaveAccessTokenMock implements SaveAccessToken {
+  accessToken: string
+
+  async save (accessToken: string): Promise<void> {
+    this.accessToken = accessToken
+  }
+}
