f681ee467ba160c9697a59411383644ebe7b16a4
lucasbcosta92 <lucasbcosta92@gmail.com>
2024-12-31 16:48:09 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient return 404

--- src/data/use-cases/add-account/remote-add-account.spec.ts
+++ src/data/use-cases/add-account/remote-add-account.spec.ts
@@ -69,6 +69,18 @@ describe('RemoteAddAccoutn', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
+  test('should throw UnexpectedError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+
+    const promise = sut.add(mockAddAccount())
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
   test('should throw UnexpectedError if HttpPostClient returns 500', async () => {
     const { sut, httpPostClientSpy } = makeSut()
 
