ec665cb6b5bf3b4d73630b6421c4419b3375258d
lucasbcosta92 <lucasbcosta92@gmail.com>
2025-01-01 13:39:11 -0300

    test: ensure Signup change name status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,32 +1,62 @@
 import React from 'react'
-import { render, type RenderResult } from '@testing-library/react'
+import { cleanup, fireEvent, render, type RenderResult } from '@testing-library/react'
+import { faker } from '@faker-js/faker'
 
-import { Helper } from '@/presentation/test'
+import { Helper, ValidationStub } from '@/presentation/test'
 import { Signup } from '@/presentation/pages'
 
 type SutTypes = {
   sut: RenderResult
 }
 
-const makeSut = (): SutTypes => {
+type SutParams = {
+  validationError: string
+}
+
+const makeSut = (params: SutParams): SutTypes => {
+  const validationStub = new ValidationStub()
+
+  validationStub.errorMessage = params?.validationError
+
   const sut = render(
-    <Signup />
+    <Signup validation={validationStub} />
   )
 
   return { sut }
 }
 
-describe('Signup', () => {
-  test('Should start with initial state', () => {
-    const validationError = 'Required field'
+const populateField = (
+  sut: RenderResult,
+  fieldName: string,
+  value = faker.lorem.word()
+): void => {
+  const input = sut.getByTestId(fieldName)
+  fireEvent.input(input, { target: { value } })
+}
 
-    const { sut } = makeSut()
+describe('Signup', () => {
+  afterEach(cleanup)
+
+  test('Should start with initial state', () => {
+    const validationError = faker.lorem.sentence(3)
+
+    const { sut } = makeSut({ validationError })
 
     Helper.testChildCount(sut, 'form-status', 0)
     Helper.testButtonIsDisabled(sut, 'submit', true)
     Helper.testStatusForField(sut, 'name-status', validationError)
-    Helper.testStatusForField(sut, 'email-status', validationError)
-    Helper.testStatusForField(sut, 'password-status', validationError)
-    Helper.testStatusForField(sut, 'passwordConfirmation-status', validationError)
+    Helper.testStatusForField(sut, 'email-status', 'Required field')
+    Helper.testStatusForField(sut, 'password-status', 'Required field')
+    Helper.testStatusForField(sut, 'passwordConfirmation-status', 'Required field')
+  })
+
+  test('should show name error if validation fails', () => {
+    const validationError = faker.lorem.sentence(3)
+
+    const { sut } = makeSut({ validationError })
+
+    populateField(sut, 'name')
+
+    Helper.testStatusForField(sut, 'name-status', validationError)
   })
 })
